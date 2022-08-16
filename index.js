7fff71920f080da75e562a51492c0eba9f81ec74
henry-thiongi <boblilhot@gmail.com>
2022-08-16 22:57:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1*num2
+let random=Math.floor(Math.random()) + 1;
+let num3=34
+let num4=5
+let mod = num3%num4
+const max = Math.max(2,5,3,20) 
